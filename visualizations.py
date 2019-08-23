85a085b9546b62f32e392f78ed9a534e38fc61fe
michellejkim <michellekimphd@gmail.com>
2019-08-23 12:42:07 -0400

    visualizations.py

--- visualizations.py
+++ visualizations.py
@@ -1,14 +1,16 @@
 """
-This module is for your final visualization code.
-One visualization per hypothesis question is required.
-A framework for each type of visualization is provided.
+This module is for your final visualization code
+One visualization per hypothesis question is required
+A framework for each type of visualization is provided
 """
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Set specific parameters for the visualizations
-large = 22; med = 16; small = 12
+large = 22
+med = 16
+small = 12
 params = {'axes.titlesize': large,
           'legend.fontsize': med,
           'figure.figsize': (16, 10),
@@ -21,116 +23,127 @@ plt.style.use('seaborn-whitegrid')
 sns.set_style("white")
 
 
-def overlapping_density(data, package='sns', input_cat='Role', target_vars='Salary'):
+def overlapping_density(data, input_cat='role', target_vars='salary',
+                        package='sns'):
     """
-    Function takes package name, input variables(categories), and target variable as input.
+    Function takes package name, input variables, and target variable as input.
     Returns a figure with overlaping density plots on a single set of axes.
-
-    Should be able to call this function in later visualization code.
-
-    PARAMETERS
-
-    :param package:        should only take sns or matplotlib as inputs, any other value should throw an error
-    :param input_vars:     should take the x variables/categories you want to plot
-    :param target_vars:    the y variable of your plot, what you are comparing
-    :return:               fig to be enhanced in subsequent visualization functions
     """
-    input_vars = set(list(data[input_cat]))
+    input_vars = list(set(data[input_cat]))
 
     # Set size of figure
-    fig,axes = plt.subplots(figsize=(16, 10), dpi=80)
+    fig, axes = plt.subplots(figsize=(16, 10), dpi=80)
 
     if package == "sns":
         for variable in input_vars:
-            sns.kdeplot(data[target_vars], hue=input_vars)
-       
+            sns.kdeplot(data[data[input_cat] == variable][target_vars],
+                        label=variable, figure=fig)
+
     elif package == 'matplotlib':
         for variable in input_vars:
-            plt.hist(clean[target_vars], density=True, label=variable, figure = fig)
+            plt.hist(data[data[input_cat] == variable][target_vars],
+                     density=True, label=variable, figure=fig)
+
+    plt.title('H1B '+target_vars.title()+' Distribution by '
+              + input_vars.title())
+    plt.xlabel(target_vars.title())
+    plt.ylabel('Density')
+    plt.legend(fontsize=10)
 
     return fig
 
 
-def distribution_timeseries(data, input_vars='Role', target_vars='Salary'):
-    """ Function that returns a Seaborn timeseries of target_variable, subset by categories"""
-    fig = plt.figure(figsize = (12,10))
-    
-    ax = sns.lineplot(x="Year", y=target_vars, data=data, hue=input_vars)
-    plt.ylabel('Salary (K)')
-    plt.xlabel('Year')
-    
-    locs,labels = plt.yticks()
-    ticks = ax.get_yticks()
-    plt.yticks(locs, ticks/1000)
-    
-    plt.title('H1B '+target_vars+' Information by '+input_vars)
-    plt.legend(ncol = 3, fontsize = 11, loc='upper center')
-    
+def distribution_timeseries(data, time_cat='year', input_cats='role',
+                            target_vars='salary'):
+    """Function that returns timeseries of target_variable, by categories"""
+    input_vars = list(set(data[input_cat]))
+
+    fig = plt.figure(figsize=(12, 10), dpi=80)
+    for variable in input_vars:
+        sns.lineplot(x=time_cat, y=target_vars, data=data, hue=input_cat,
+                     figure=fig)
+
+    if target_vars == 'salary':
+        locs, labels = plt.yticks()
+        ticks = ax.get_yticks()
+        plt.yticks(locs, ticks/1000)
+        plt.ylabel('Salary (K)')
+    else:
+        plt.ylabel(target_vars.title())
+
+    plt.xlabel(input_cats.title())
+    plt.title('H1B '+target_vars.title()+' Information by '+input_vars.title())
+    plt.legend(ncol=3, fontsize=11)
+
     return fig
 
-def boxplot_plot(data, package='sns', input_cat='Role', target_vars='Salary'):
-    """
-    Same specifications and requirements as overlapping density plot
 
-    Function takes package name, input variables(categories), and target variable as input.
-    Returns a figure
+def boxplot_plot(data, grouping='year', input_cat='role', target_vars='salary',
+                 package='sns'):
+    """ Same specifications and requirements as overlapping density plot."""
 
-    PARAMETERS
-
-    :param package:        should only take sns or matplotlib as inputs, any other value should throw and error
-    :param input_vars:     should take the x variables/categories you want to plot
-    :param target_vars:    the y variable of your plot, what you are comparing
-    :return:               fig to be enhanced in subsequent visualization functions
-    """
-  input_vars = set(list(data[input_cat]))
+    input_vars = set(data[input_cat])
 
     # Set size of figure
     fig = plt.figure(figsize=(16, 10), dpi=80)
 
     if package == "sns":
+        ax = sns.boxplot(x=grouping, y=target_vars, hue=input_cat, data=data)
+    elif package == 'matplotlib':
         for variable in input_vars:
-            sns.boxplot(data[target_vars], hue=input_vars)
+            plt.box(data[data[input_vars] == variable][target_vars],
+                    label=variable, linewidth=None, color=None, figure=fig)
+    plt.legend(fontsize=11)
+    plt.ylabel(target_vars.title())
+    plt.xlabel(grouping.title())
 
-       
-    elif package == 'matplotlib': # apparently want a line plot here?
-        for variable in input_vars:
-            plt.box(data[data[input_vars]==variable][target_vars], label=variable, linewidth=None, color=None, figure = fig)
+    if ((grouping == 'role') | (grouping == 'region') |
+    (grouping == 'industry')):
+        for item in ax.get_xticklabels():
+            item.set_rotation(45)
+            plt.xlabel(grouping.title(), fontsize=8)
 
+    plt.title(target_vars.title()+' by '+input_cat.title()+', grouped by '
+              + grouping.title())
     return fig
 
 
-def visualization_one(data, target_var = 'Salary', input_vars= 'Role', output_image_name='Salary_by_Role'):
+def visualization_one(data, output_image_name='Salary_Distribution_by_Role'):
     """
-    The visualization functions are what is used to create each individual image.
-    The function should be repeatable if not generalizable
-    The function will call either the boxplot or density plot functions you wrote above
-
     :param target_var:
     :param input_vars:
     :param output_image_name: the desired name for the image saved
     :return: outputs a saved png file and returns a fig object for testing
     """
-    overlapping_density
-                          
 
-    # Starter code for labeling the image
-    plt.xlabel(None, figure = fig)
-    plt.ylabel(None, figure = fig)
-    plt.title(None, figure= fig)
-    plt.legend()
+    fig = overlapping_density(data, input_cat='role', target_vars='salary')
+
+    plt.xlabel(target_vars.title(), figure=fig)
+    plt.ylabel('Density', figure=fig)
+#     plt.title(target_vars.title()+' Distribution by '+input_vars.title(), figure=fig)
+    plt.legend(fontsize=10)
+    plt.show()
 
     # exporting the image to the img folder
-    plt.savefig(f'img/{output_image_name}.png', transparent = True, figure = fig)
+    plt.savefig(f'img/{output_image_name}.png', transparent=True,
+                figure=fig)
     return fig
 
 
-# please fully flesh out this function to meet same specifications of visualization one
+def visualization_two(data, target_var='salary', input_vars='region',
+                      output_image_name='Salary_by_Region'):
+    fig = overlapping_density(data, input_cat=input_vars,
+                              target_vars=target_var)
+    plt.savefig(f'img/{output_image_name}.png', transparent=True,
+                figure=fig)
 
-def visualization_two(output_image_name):
-    pass
+    return fig
+
 
 def visualization_three(output_image_name):
-    pass
+    fig = distribution_timeseries(data)
+    return fig
+
 
 def visualization_four(output_image_name):
     pass
